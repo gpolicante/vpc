5ddd65ac068ff0fdb96be9160d870b359c7ed653
gabriel.policante <gabriel.policante@mercadolivre.com>
2021-04-19 23:12:42 -0300

    vai

--- rt_association.tf
+++ rt_association.tf
@@ -14,3 +14,55 @@ resource "aws_route_table_association" "rt_public_association_second" {
   route_table_id = aws_route_table.rt_public.id
   depends_on = [aws_route_table.rt_public, aws_route_table.rt_public, aws_internet_gateway.gw ]
 }
+
+
+resource "aws_route_table_association" "rt_public_association_thr" {
+  
+  subnet_id      =  aws_subnet.subnet_thr_public.id
+  route_table_id = aws_route_table.rt_public.id
+  depends_on = [aws_route_table.rt_public, aws_route_table.rt_public, aws_internet_gateway.gw ]
+}
+
+
+resource "aws_route_table_association" "rt_public_association_quarter" {
+  
+  subnet_id      =  aws_subnet.subnet_quarter_public.id
+  route_table_id = aws_route_table.rt_public.id
+  depends_on = [aws_route_table.rt_public, aws_route_table.rt_public, aws_internet_gateway.gw ]
+}
+
+
+
+### privada
+
+
+resource "aws_route_table_association" "rt_private_association_primary" {
+  
+  subnet_id      =  aws_subnet.subnet_primary.id
+  route_table_id = aws_route_table.rt_private.id
+  depends_on = [aws_route_table.rt_private, aws_route_table.rt_private, aws_internet_gateway.gw ]
+}
+
+
+resource "aws_route_table_association" "rt_private_association_second" {
+  
+  subnet_id      =  aws_subnet.subnet_second.id
+  route_table_id = aws_route_table.rt_private.id
+  depends_on = [aws_route_table.rt_private, aws_route_table.rt_private, aws_internet_gateway.gw ]
+}
+
+
+resource "aws_route_table_association" "rt_private_association_thr" {
+  
+  subnet_id      =  aws_subnet.subnet_thr.id
+  route_table_id = aws_route_table.rt_private.id
+  depends_on = [aws_route_table.rt_private, aws_route_table.rt_private, aws_internet_gateway.gw ]
+}
+
+
+resource "aws_route_table_association" "rt_private_association_quarter" {
+  
+  subnet_id      =  aws_subnet.subnet_quarter.id
+  route_table_id = aws_route_table.rt_private.id
+  depends_on = [aws_route_table.rt_private, aws_route_table.rt_private, aws_internet_gateway.gw ]
+}
