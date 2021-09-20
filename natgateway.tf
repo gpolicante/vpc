142778b11c6e7cb8fd40542f4bd0983719954c6c
gpolicante <gabrielpolicante22@gmail.com>
2021-09-20 17:33:24 -0300

    vai

--- natgateway.tf
+++ natgateway.tf
@@ -1,6 +1,6 @@
 resource "aws_nat_gateway" "gw" {
   allocation_id =  aws_eip.allocation_natgw.id
-  subnet_id     =  aws_subnet.subnet_primary.id
+  subnet_id     =  aws_subnet.subnet_primary_public.id
 
   tags = var.tags
   depends_on = [ aws_eip.allocation_natgw, aws_subnet.subnet_primary, aws_internet_gateway.gw ]
